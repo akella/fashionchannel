c0eb35ad65a25e34b2e99e08dd03cf52ffa80fb9
Yevgen <godun.yevgen@gmail.com>
2012-09-04 17:51:53 +0300

    Index done

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+// -------------------- big banner scroll ---------------------------- //
+	$('.big-banner').scrollable({
+			next:'.next',
+			prev:'.prev'
+	}).autoscroll({ autoplay: true });
+	api = $('.big-banner').data('scrollable');
+	$('.tab-big-banner a').hover(function() {
+		$('.tab-big-banner a').removeClass("tab-big-banner__active");
+		$(this).addClass("tab-big-banner__active");
+	    ar = $(this).attr('href');
+	    api.seekTo(ar, 300);
+	    return false;
+	});
+	$('.tab-big-banner a').click(function() {
+		return false;
+	});
 // -------------------- entry slider ---------------------------- //
 	$('.entry-slider').scrollable({
 			next:'.entry-slider-nav__next',
